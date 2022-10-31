0cb631e94e0ed59d4686a65baf149e4f69b632ff
Amelye Chatila <amechax@gmail.com>
2022-10-31 05:54:08 +0300

    first commit

--- src/App.js
+++ src/App.js
@@ -1,25 +1,47 @@
-import logo from './logo.svg';
-import './App.css';
+import React, { useRef, useState } from 'react'
+import { Canvas, useFrame, useLoader } from '@react-three/fiber'
+import { OrbitControls } from '@react-three/drei'
+import { TextureLoader } from 'three/src/loaders/TextureLoader'
 
-function App() {
+function Box(props) {
+  const mesh = useRef(null)
+  const [hovered, setHover] = useState(false)
+  const [active, setActive] = useState(false)
+  useFrame((state, delta) => (mesh.current.rotation.x += 0.01))
+  const colorMap1 = useLoader(TextureLoader, 'images/amelye.jpg')
+  const colorMap2 = useLoader(TextureLoader, 'images/founder.jpg')
+  const colorMap3 = useLoader(TextureLoader, 'images/amelye.jpg')
+  const colorMap4 = useLoader(TextureLoader, 'images/blank.jpg')
+  const colorMap5 = useLoader(TextureLoader, 'images/blank.jpg')
+  const colorMap6 = useLoader(TextureLoader, 'images/amelye.jpg')
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+    <mesh
+      {...props}
+      ref={mesh}
+      scale={active ? 2.5 : 1}
+      onClick={(event) => setActive(!active)}
+      onPointerOver={(event) => setHover(true)}
+      onPointerOut={(event) => setHover(false)}>
+      <boxGeometry args={[1, 1, 1]} />
+      <meshStandardMaterial attach="material-1" map={colorMap1} />
+      <meshStandardMaterial attach="material-2" map={props.colorMap ? props.colorMap1 : colorMap2 } />
+      <meshStandardMaterial attach="material-3" map={colorMap3} />
+      <meshStandardMaterial attach="material-4" map={colorMap4} />
+      <meshStandardMaterial attach="material-5" map={colorMap5} />
+      <meshStandardMaterial attach="material-6" map={colorMap6} />
+    </mesh>
+  )
 }
 
-export default App;
+export default function App() {
+  return (
+    <Canvas>
+      <ambientLight />
+      <pointLight position={[10, 10, 10]} />
+      <Box position={[-1.2, 0, 0]} colorMap={useLoader(TextureLoader, 'images/blank.jpg')} />
+      <Box position={[1.2, 0, 0]} />
+      <OrbitControls />
+    </Canvas>
+  )
+}
